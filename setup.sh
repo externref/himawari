08fddfedeea221f2303dc7f24279d3924278b6ff
sarthak-py <shiva02939@gmail.com>
2022-04-12 21:55:33 +0530

    added a shell file for quick setups for my device.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+python3 -m pip install virtualenv
+python3 -m virtualenv env
+source env/bin/activate
+python3 -m pip install lightbulb-ext-filament
+python3 -m pip install -U hikari hikari-lightbulb
+python3 .
