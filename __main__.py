9151360cfcbc8df52c286bf172bb9e769cdf0efa
sarthak-py <shiva02939@gmail.com>
2022-04-14 12:07:12 +0530

    added new attrs to bot for version info

--- __main__.py
+++ __main__.py
@@ -1,4 +1,8 @@
 from src.core.bot import Gojo
 
 if __name__ == "__main__":
-    Gojo().run()
+    bot = Gojo()
+    bot.__version__ = "0.0.1"
+    bot.hikari_version = __import__("hikari").__version__
+    bot.lightbulb_version = __import__("lightbulb").__version__
+    bot.run()
