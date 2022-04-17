7c6fdd3b5a4a4b57375522a48c607a01bfa728c1
sarthak-py <shiva02939@gmail.com>
2022-04-17 12:23:41 +0530

    update requirements

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 python3 -m pip install virtualenv
 python3 -m virtualenv env
 source env/bin/activate
-python3 -m pip install lightbulb-ext-filament
+python3 -m pip install lightbulb-ext-filament aiosqlite python-dotenv groundwork-sphinx-theme sphinx myst-parser
 python3 -m pip install -U hikari hikari-lightbulb
 python3 .
