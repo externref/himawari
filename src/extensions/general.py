24d9590e0cc5799a34e574e1555a224771c2200d
sarthak-py <shiva02939@gmail.com>
2022-04-19 15:37:05 +0530

    avatar command

--- src/extensions/general.py
+++ src/extensions/general.py
@@ -7,6 +7,7 @@ from lightbulb.context.slash import SlashContext
 from lightbulb.commands.slash import SlashCommand
 from lightbulb.decorators import command, option, implements
 
+from hikari.users import User
 from hikari.embeds import Embed
 
 from ..core.bot import Gojo
@@ -15,7 +16,7 @@ from ..core.bot import Gojo
 class General(Plugin):
     def __init__(self):
         self.bot: Gojo
-        super().__init__(name="general", description="General bot commands.")
+        super().__init__(name="general", description="General bot commands.", default_enabled_guilds=(964157647752400916) )
 
 
 general = General()
@@ -36,6 +37,18 @@ async def _ping(context: SlashContext) -> None:
     embed.description = f"Bot Latency: `{round(general.bot.heartbeat_latency*1000,2)} ms`\nDatabase Latency: `{prefix_fetch} ms`"
     await context.edit_last_response(embed=embed)
 
+@general.command
+@option(name="user", description="User to check avatar of.", required=False, type=User)
+@command(name="avatar", description="Check avatar of a user.")
+@implements(SlashCommand)
+async def av_command(context: SlashContext      ) -> None:
+    target = context.options.user or context.author
+    await context.respond(
+        embed= Embed(
+            color= await general.bot.color_for(context.get_guild()),
+            description=f"[Download Avatar]({target.avatar_url or target.default_avatar_url})"
+        ).set_author(name=f"{target.username.replace('_','_/')}'s avatar").set_footer(text=f"Requested by {context.author}").set_image(target.display_avatar_url)
+    )
 
 def load(bot: Gojo):
     bot.add_plugin(general)
