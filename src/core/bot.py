e27dff8dbf6a7b069c7160ebb25bee85a43b979f
sarthak-py <shiva02939@gmail.com>
2022-04-21 12:13:35 +0530

    documented bot.py

--- src/core/bot.py
+++ src/core/bot.py
@@ -16,31 +16,33 @@ from ..database.welcome import WelcomerHandler
 
 def get_token():
     import dotenv
+    # getting the `TOKEN` enviornment variable from .env file
 
     dotenv.load_dotenv()
     return os.getenv("TOKEN")
 
 
 class Gojo(lightbulb.BotApp):
+    """Initialising the bot class with all additional methods and attributes."""
     def __init__(self):
-        self.color_handler = ColorHandler()
-        self.goodbye_handler = GoodbyeHandler()
-        self.welcome_handler = WelcomerHandler()
-        self.starboard_handler = StarboardHandler()
+        self.color_handler = ColorHandler()         # color handler to deal with bot's custom embed colors.
+        self.goodbye_handler = GoodbyeHandler()     # goodbye database connection for MemberDeleteEvent events.
+        self.welcome_handler = WelcomerHandler()    # welcomer database connection for greeting new users.
+        self.starboard_handler = StarboardHandler() # starboard manager with required methods.
         super().__init__(
             token=get_token(),
             help_slash_command=True,
             #default_enabled_guilds=(os.getenv("GUILD_ID"),),
             intents=(
-                hikari.Intents.ALL_UNPRIVILEGED
-                | hikari.Intents.GUILD_MEMBERS
-                | hikari.Intents.GUILD_PRESENCES
+                hikari.Intents.ALL_UNPRIVILEGED     # adding all not privilaged intents to the bot
+                | hikari.Intents.GUILD_MEMBERS      # GUILD_MEMBERS intents for welcome & leave module
+                | hikari.Intents.GUILD_PRESENCES    # GUILD_PRESENCES intents for other utils.
             ),
             banner="hikari",
         )
         tasks.load(self)
         self._boot_time = datetime.now()
-        self.load_extensions_from("src/extensions")
+        self.load_extensions_from("src/extensions") # loading all plugins and help command from the extensions folder.
         self.invite_url = (
             "https://discord.com/api/oauth2/authorize?client_id=961613807564775447"
             + "&permissions=378025593921&scope=bot%20applications.commands"
@@ -48,6 +50,7 @@ class Gojo(lightbulb.BotApp):
         self.event_manager.subscribe(hikari.StartingEvent, self.start_handlers)
 
     async def start_handlers(self, _: hikari.StartingEvent) -> None:
+        """Additional setups which need to be called under async functions."""
         self.custom_session = aiohttp.ClientSession()
         await self.color_handler.setup()
         await self.goodbye_handler.setup()
