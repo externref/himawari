acfe8a19f76a8e31e71aaceb1df124db584670af
sarthak-py <shiva02939@gmail.com>
2022-04-22 12:02:52 +0530

    added database backup, sending messages on new server add/remove

--- src/extensions/dev_core.py
+++ src/extensions/dev_core.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+
 import hikari
 import lightbulb
 
@@ -22,8 +24,8 @@ class Developer(lightbulb.Plugin):
 dev = Developer()
 
 
-@tasks.task(m=10, auto_start=True)
-async def updated_status() -> None:
+@tasks.task(m=10)
+async def update_status() -> None:
     await dev.bot.update_presence(
         activity=Activity(
             type=ActivityType.LISTENING,
@@ -31,6 +33,58 @@ async def updated_status() -> None:
         ),
         status=Status.IDLE,
     )
+
+
+@tasks.task(m=10)
+async def send_db_backup() -> None:
+    await dev.bot.rest.create_message(
+        os.getenv("BACKUP_CHANNEL"),
+        attachments=[
+            hikari.File(db_file)
+            for db_file in (os.listdir())
+            if db_file.endswith(".db")
+        ],
+    )
+
+
+@dev.listener(hikari.GuildJoinEvent)
+async def new_guild(event: hikari.GuildJoinEvent) -> None:
+    embed = hikari.Embed(color=0x3FFF5C, title=f"+ {event.guild.name}")
+    embed.set_thumbnail(event.guild.icon_url)
+    embed.add_field(name="Guild ID", value=str(event.guild_id))
+    embed.add_field(
+        name="OWNER/ID",
+        value=f"{await dev.bot.getch_user(event.guild.owner_id)} | {event.guild.owner_id}",
+    )
+    embed.add_field(name="Member Count", value=len(event.guild.get_members()))
+    embed.add_field(
+        name="Bot Count",
+        value=len([m for m in event.guild.get_members().values() if m.is_bot]),
+    )
+    await dev.bot.rest.create_message(os.getenv("SERVER_LOG_CHANNEL"), embed=embed)
+
+
+@dev.listener(hikari.GuildLeaveEvent)
+async def new_guild(event: hikari.GuildLeaveEvent) -> None:
+    embed = hikari.Embed(color=0xFF0000, title=f"- {event.guild.name}")
+    embed.set_thumbnail(event.old_guild.icon_url)
+    embed.add_field(name="Guild ID", value=str(event.guild_id))
+    embed.add_field(
+        name="OWNER/ID",
+        value=f"{await dev.bot.getch_user(event.old_guild.owner_id)} | {event.old_guild.owner_id}",
+    )
+    embed.add_field(name="Member Count", value=len(event.old_guild.get_members()))
+    embed.add_field(
+        name="Bot Count",
+        value=len([m for m in event.old_guild.get_members().values() if m.is_bot]),
+    )
+    await dev.bot.rest.create_message(os.getenv("SERVER_LOG_CHANNEL"), embed=embed)
+
+
+@dev.listener(hikari.StartedEvent)
+async def start_tasks(_: hikari.StartedEvent) -> None:
+    update_status.start()
+    send_db_backup.start()
 
 
 @dev.listener(lightbulb.SlashCommandErrorEvent)
@@ -91,6 +145,7 @@ async def lightbulb_error(
 
 def load(bot: Gojo):
     bot.add_plugin(dev)
+
 
 def unload(bot: Gojo):
     bot.remove_plugin(dev)
